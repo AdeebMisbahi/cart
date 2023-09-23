52761968bfb337213ac3039cfb03b81ffc19493b
AdeebMisbahi1 <130211733+AdeebMisbahi@users.noreply.github.com>
2023-09-23 20:09:49 +0530

    how event works my first onclick event

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,16 +1,35 @@
 import React from "react";
 
 class CartItem extends React.Component {
+      constructor(){
+        super();
+        this.state={
+            price: 999,
+            title: 'Phone',
+            qty: 1,
+            img: ''
+        }
+      }
+    increaseQuantity(){
+        console.log('test')
+      }
+      
     render(){
+          const {price , title, qty,  }= this.state;
          return(
          <div className="cart-item">
             <div className="left-block">
                <img style={styles.image} alt=""/>
             </div>
          <div className="right-block">
-            <div style={ {fontSize: 25} }> Phone</div>
-            <div style={ {color :'#777'} }>Rs 1999</div>
-            <div style={ {color :'#777'} }>Qty: 1</div>
+            <div style={ {fontSize: 25} }> {title}</div>
+            <div style={ {color :'#777'} }>Rs {price}</div>
+            <div style={ {color :'#777'} }>Qty: {qty}</div>
+            <div className="cart-item-actions">
+                <img className="action-icons" src="https://cdn-icons-png.flaticon.com/128/1828/1828926.png" onClick={this.increaseQuantity /* reference of increaseQuantity */} alt=""></img>
+                <img className="action-icons" src="https://cdn-icons-png.flaticon.com/128/992/992683.png" alt=""></img>
+                <img className="action-icons" src="https://cdn-icons-png.flaticon.com/128/484/484662.png" alt=""></img>
+            </div>
          </div>
          </div>
          )
