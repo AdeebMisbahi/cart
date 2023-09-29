642556e46a30cbb217a8e4c94d02ece86182b5d1
AdeebMisbahi1 <130211733+AdeebMisbahi@users.noreply.github.com>
2023-09-29 07:27:39 +0530

    added another Cart Component and inside it we used cartitem component

--- src/CartItem.js
+++ src/CartItem.js
@@ -11,19 +11,34 @@ class CartItem extends React.Component {
         }
         // Bind 'this' to the increaseQuantity method
     // this.increaseQuantity = this.increaseQuantity.bind(this);
-      }
-      
-    increaseQuantity=()=>{
-        // this.setState({
-        //     qty: this.state.qty + 1,
-        //   });
+    }
+    
+      increaseQuantity = () => {
+    /*  
+        this.setState({
+        qty: this.state.qty + 1,
+        });
+    */
+
 // second setState while passing callback function instead of object
         this.setState((prevState) => ({
-            qty: prevState.qty + 1,
-            }));
+          qty: prevState.qty + 1,
+        }), () => {
+          // This callback function will be executed after the state is updated and the component is re-rendered
+          console.log(this.state);
+        });
       }
-      
+    decreaseQuantity=()=>{
+        const {qty}=this.state;
+        if(qty===0){
+            return;
+        }
+        this.setState((prevState)=>({
+            qty:prevState.qty-1
+        }))
+    }
     render(){
+        console.log('render')
           const {price , title, qty,  }= this.state;
          return(
          <div className="cart-item">
@@ -36,8 +51,8 @@ class CartItem extends React.Component {
             <div style={ {color :'#777'} }>Qty: {qty}</div>
             <div className="cart-item-actions">
                 <img className="action-icons" src="https://cdn-icons-png.flaticon.com/128/1828/1828926.png" onClick={this.increaseQuantity /* reference of increaseQuantity */} alt=""></img>
-                <img className="action-icons" src="https://cdn-icons-png.flaticon.com/128/992/992683.png" alt=""></img>
-                <img className="action-icons" src="https://cdn-icons-png.flaticon.com/128/484/484662.png" alt=""></img>
+                <img className="action-icons" src="https://cdn-icons-png.flaticon.com/128/992/992683.png" onClick={this.decreaseQuantity} alt=""></img>
+                <img className="action-icons" src="https://cdn-icons-png.flaticon.com/128/484/484662.png" onClick={this.handleClick} alt=""></img>
             </div>
          </div>
          </div>
